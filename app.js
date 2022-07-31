21a5e11cef71315b602284a0f43768977b21a73e
apontillas <angelypontillas13@gmail.com>
2022-07-31 11:18:56 -0400

    Add Level 3 Security

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5")
+
 
 const app = express();
 
-console.log(process.env.API_KEY);
+console.log(md5(12345));
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -24,7 +25,6 @@ const userSchema = new mongoose.Schema ({
 });
 
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 
 const User = new mongoose.model("User", userSchema);
@@ -44,7 +44,7 @@ app.route("/login")
 
 .post((req, res) => {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, (err, foundUser) => {
     if(!err) {
@@ -70,7 +70,7 @@ app.route("/register")
 .post((req, res) => {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(err => {
@@ -81,7 +81,6 @@ app.route("/register")
     }
   })
 })
-
 
 
 
